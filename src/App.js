518d9ef326551b7e0f91223cb993ed65cd4c4971
Vipul-J <jujarvipul21@gmail.com>
2023-08-09 12:31:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';  
+import LoginPage from './components/LoginPage';
+import EventLogin from './components/EventAdd';
+import RegisterPage from './components/RegisterPage';
+import Explore from './pages/Explore';
+import EventDetailOne from './pages/EventDetailOne';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} /> 
+        <Route exact path="/explore" element={<Explore />} /> 
+        <Route exact path="/eventdetailsone" element={<EventDetailOne />} />  
+        <Route exact path="/profile" element={<Profile />} /> 
+        <Route exact path="/login" element={<LoginPage />} /> 
+        <Route exact  path="/register" element={<RegisterPage />} /> 
+        <Route exact path="/evepage" element={<EventLogin />} />
+      </Routes>
+    </Router>
   );
 }
 
